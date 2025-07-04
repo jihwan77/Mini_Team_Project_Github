73c98afbb2e04eee6c5dd6d1ca5899090928c1d7
Gill <qudrlftm0104@gmail.com>
2025-07-04 15:54:43 +0900

    domain/DB

--- gitProject/src/model/domain/Product.java
+++ gitProject/src/model/domain/Product.java
@@ -1,5 +1,19 @@
 package model.domain;
 
-public class Product {
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Product {
+	private int id;
+	private String name;
+	private int price;
+	private int kcal;
+	private int gram;
+	private String catagory;
 }
--- gitProject/src/model/domain/ShoppingCart.java
+++ gitProject/src/model/domain/ShoppingCart.java
@@ -1,5 +1,31 @@
 package model.domain;
 
-public class ShoppingCart {
+import java.util.ArrayList;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ShoppingCart {
+	private int id;
+	private String name;
+	private int price;
+	private int kcal;
+	private int gram;
+	private String catagory;
+	ArrayList<Product> cart;
+	public ShoppingCart (int id, String name, int price, int kcal, int gram, String catagory) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.kcal = kcal;
+		this.gram = gram;
+		this.catagory = catagory;
+		cart = new ArrayList<>();
+	}
 }
